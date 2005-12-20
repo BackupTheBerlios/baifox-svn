dfdeb8586c086848a14b06ad07adf263c5f8493d
fidojones <fidojones@51d618aa-2c00-0410-be48-da876a5fb5c8>
2005-12-20 23:54:02 +0000

    primeras pruebas de panel de usuario

---
+++ trunk/panel/user_panel/index.php
@@ -0,0 +1,150 @@
+<html>
+<head>
+<title>Untitled Document</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+</head>
+
+<body bgcolor="#FFFFFF" text="#000000">
+<table width="80%" border="0" cellpadding="0" cellspacing="0" align="center">
+  <tr> 
+    <td width="18%" rowspan="2"><img src="images/logo_baifox.gif" width="147" height="107"></td>
+    <td width="82%" height="71">
+      <table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr> 
+          <td bgcolor="#CD3500" width="82%"><img src="images/img_titulo.gif" width="257" height="79"></td>
+          <td bgcolor="#D65300" width="11%">&nbsp;</td>
+          <td bgcolor="#E27400" width="5%">&nbsp;</td>
+          <td bgcolor="#F2A500" width="2%">&nbsp;</td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+  <tr>
+    <td width="82%" bgcolor="#949494">
+      <table width="100%" border="0" cellspacing="0" cellpadding="0">
+        <tr align="center"> 
+          <td width="150"><b><font face="Arial, Helvetica, sans-serif" size="2" color="#FFFFFF">[ 
+            inicio ]</font></b></td>
+          <td width="150"><b><font face="Arial, Helvetica, sans-serif" size="2" color="#FFFFFF">[ 
+            ayuda ]</font></b></td>
+          <td width="150"><b><font face="Arial, Helvetica, sans-serif" size="2" color="#FFFFFF">[ 
+            documentaci&oacute;n ]</font></b></td>
+          <td width="150"><b><font face="Arial, Helvetica, sans-serif" size="2" color="#FFFFFF">[ 
+            cerrar sesi&oacute;n ] </font></b></td>
+          <td width="150"><b></b></td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
+<br>
+<table width="80%" border="0" cellspacing="0" cellpadding="0" align="center" height="400">
+  <tr valign="top"> 
+    <td width="57%"> <br>
+      <table width="95%" border="0" cellspacing="0" cellpadding="0" align="center">
+        <tr> 
+          <td colspan="3" bgcolor="#E27400"> 
+            <table width="100%" border="0" cellspacing="0" cellpadding="0">
+              <tr> 
+                <td width="12%" align="center"><img src="images/img_mundo.gif" width="39" height="39"></td>
+                <td width="88%"><font face="Arial, Helvetica, sans-serif" size="1"><b><font size="3" color="#FFFFFF">Dominios</font><font size="2" color="#FFFFFF"><br>
+                  dominios alojados</font></b></font></td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+        <tr align="center"> 
+          <td width="61%" bgcolor="#D6D6D6"><font face="Arial, Helvetica, sans-serif" size="2">nombre</font></td>
+          <td width="18%" bgcolor="#D6D6D6"><font face="Arial, Helvetica, sans-serif" size="2">disco 
+            usado</font></td>
+          <td width="21%" bgcolor="#D6D6D6"><font face="Arial, Helvetica, sans-serif" size="2">tr&aacute;fico</font></td>
+        </tr>
+        <tr align="center"> 
+          <td width="61%">&nbsp;</td>
+          <td width="18%">&nbsp;</td>
+          <td width="21%">&nbsp;</td>
+        </tr>
+        <tr align="center"> 
+          <td colspan="2"><font face="Arial, Helvetica, sans-serif" size="2"><b>0 
+            Dominios en total</b></font></td>
+          <td width="21%">&nbsp;</td>
+        </tr>
+      </table>
+    </td>
+    <td width="4%">&nbsp;</td>
+    <td width="39%"> <br>
+      <table width="266" border="0" cellspacing="0" cellpadding="0" align="center">
+        <tr> 
+          <td colspan="3" valign="top"> 
+            <table width="100%" border="0" cellspacing="0" cellpadding="0" background="images/fnd_disco.gif">
+              <tr> 
+                <td width="15%" align="center">&nbsp;</td>
+                <td width="85%"><font face="Arial, Helvetica, sans-serif" size="1"><b><font size="3" color="#FFFFFF">Espacion 
+                  en disco</font><font size="2" color="#FFFFFF"><br>
+                  consumo y dipsponibilidad</font></b></font></td>
+              </tr>
+            </table>
+            <table width="100%" border="0" cellspacing="5" cellpadding="0">
+              <tr align="center"> 
+                <td width="46%" align="left"><b><font face="Arial, Helvetica, sans-serif" size="2">&nbsp;Espacio 
+                  en disco</font></b></td>
+                <td width="34%"><img src="images/barra_progreso.gif" width="120" height="11"></td>
+              </tr>
+              <tr align="center"> 
+                <td width="46%" align="left"><b><font face="Arial, Helvetica, sans-serif" size="2">&nbsp;Espacio 
+                  total </font></b></td>
+                <td width="34%" align="right"><b><font face="Arial, Helvetica, sans-serif" size="2">500&nbsp;</font></b></td>
+              </tr>
+              <tr align="center"> 
+                <td width="46%" align="left"><b><font face="Arial, Helvetica, sans-serif" size="2">&nbsp;Espacio 
+                  Usado</font></b></td>
+                <td width="34%" align="right"><b><font face="Arial, Helvetica, sans-serif" size="2">50&nbsp;</font></b></td>
+              </tr>
+              <tr align="center"> 
+                <td width="46%" align="left"><b><font face="Arial, Helvetica, sans-serif" size="2">&nbsp;Disponible</font></b></td>
+                <td width="34%" align="right"><b><font face="Arial, Helvetica, sans-serif" size="2">50&nbsp;</font></b></td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+      <br>
+      <table width="266" border="0" cellspacing="0" cellpadding="0" align="center">
+        <tr> 
+          <td colspan="3" valign="top"> 
+            <table width="100%" border="0" cellspacing="0" cellpadding="0" background="images/fnd_trafico.gif">
+              <tr> 
+                <td width="15%" align="center">&nbsp;</td>
+                <td width="85%"><font face="Arial, Helvetica, sans-serif" size="1"><b><font size="3" color="#FFFFFF">Transferencia</font><font size="2" color="#FFFFFF"><br>
+                  Transferencia de Datos</font></b></font></td>
+              </tr>
+            </table>
+            <table width="100%" border="0" cellspacing="5" cellpadding="0">
+              <tr align="center"> 
+                <td width="46%" align="left"><b><font face="Arial, Helvetica, sans-serif" size="2">&nbsp;Transferencia</font></b></td>
+                <td width="34%"><img src="images/barra_progreso.gif" width="120" height="11"></td>
+              </tr>
+              <tr align="center"> 
+                <td width="46%" align="left"><b><font face="Arial, Helvetica, sans-serif" size="2">&nbsp;L&iacute;mite 
+                  Mensual </font></b></td>
+                <td width="34%" align="right"><b><font face="Arial, Helvetica, sans-serif" size="2">1000&nbsp;</font></b></td>
+              </tr>
+              <tr align="center"> 
+                <td width="46%" align="left"><b><font face="Arial, Helvetica, sans-serif" size="2">&nbsp;&Uacute;ltimo 
+                  Mes </font></b></td>
+                <td width="34%" align="right"><b><font face="Arial, Helvetica, sans-serif" size="2">50&nbsp;</font></b></td>
+              </tr>
+              <tr align="center"> 
+                <td width="46%" align="left"><b><font face="Arial, Helvetica, sans-serif" size="2">&nbsp;Mes 
+                  actual </font></b></td>
+                <td width="34%" align="right"><b><font face="Arial, Helvetica, sans-serif" size="2">50&nbsp;</font></b></td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </td>
+  </tr>
+</table>
+</body>
+</html>
