96f13ed52ed8ce6ef1cc1af6315993f1509e82e7
fidojones <fidojones@51d618aa-2c00-0410-be48-da876a5fb5c8>
2005-12-21 13:31:09 +0000

    diseños preliminares para el panel de usuarios

--- trunk/panel/user_panel/index.php
+++ trunk/panel/user_panel/index.php
@@ -2,6 +2,14 @@
 <head>
 <title>Panel Administraci&oacute;n</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link href="estilos.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+<!--
+.Estilo2 {font-size: 11px}
+.Estilo5 {font-size: 11px}
+.Estilo6 {font-size: 11px}
+-->
+</style>
 </head>
 
 <body bgcolor="#FFFFFF" text="#000000">
@@ -11,7 +19,7 @@
     <td width="82%" height="71">
       <table width="100%" border="0" cellspacing="0" cellpadding="0">
         <tr> 
-          <td bgcolor="#CD3500" width="82%"><img src="images/img_titulo.gif" width="257" height="79"></td>
+          <td bgcolor="#CD3500" width="82%"><div align="center"><img src="images/img_titulo.gif" width="257" height="79"></div></td>
           <td bgcolor="#D65300" width="11%">&nbsp;</td>
           <td bgcolor="#E27400" width="5%">&nbsp;</td>
           <td bgcolor="#F2A500" width="2%">&nbsp;</td>
@@ -23,14 +31,14 @@
     <td width="82%" bgcolor="#949494">
       <table width="100%" border="0" cellspacing="0" cellpadding="0">
         <tr align="center"> 
-          <td width="150"><b><font face="Arial, Helvetica, sans-serif" size="2" color="#FFFFFF">[ 
-            inicio ]</font></b></td>
-          <td width="150"><b><font face="Arial, Helvetica, sans-serif" size="2" color="#FFFFFF">[ 
-            ayuda ]</font></b></td>
-          <td width="150"><b><font face="Arial, Helvetica, sans-serif" size="2" color="#FFFFFF">[ 
-            documentaci&oacute;n ]</font></b></td>
-          <td width="150"><b><font face="Arial, Helvetica, sans-serif" size="2" color="#FFFFFF">[ 
-            cerrar sesi&oacute;n ] </font></b></td>
+          <td width="150"><font color="#FFFFFF">[ 
+            inicio ]</font></td>
+          <td width="150"><font color="#FFFFFF">[ 
+            ayuda ]</font></td>
+          <td width="150"><font color="#FFFFFF">[ 
+            documentaci&oacute;n ]</font></td>
+          <td width="150"><font color="#FFFFFF">[ 
+            cerrar sesi&oacute;n ] </font></td>
           <td width="150"><b></b></td>
         </tr>
       </table>
@@ -47,42 +55,42 @@
             <table width="100%" border="0" cellspacing="0" cellpadding="0">
               <tr> 
                 <td width="12%" align="center"><img src="images/img_mundo.gif" width="39" height="39"></td>
-                <td width="88%"><font face="Arial, Helvetica, sans-serif" size="1"><b><font size="3" color="#FFFFFF">Dominios</font><font size="2" color="#FFFFFF"><br>
-                  dominios alojados</font></b></font></td>
+                <td width="88%"><font face="Verdana, Arial, Helvetica, sans-serif" size="1"><b><font size="3" color="#FFFFFF">Dominios</font><font size="2" color="#FFFFFF"><br>
+                        </font></b><font size="2" color="#FFFFFF"><font size="1">dominios alojados</font></font></font></td>
               </tr>
             </table>
           </td>
         </tr>
         <tr align="center"> 
-          <td width="61%" bgcolor="#D6D6D6"><font face="Arial, Helvetica, sans-serif" size="2">nombre</font></td>
-          <td width="18%" bgcolor="#D6D6D6"><font face="Arial, Helvetica, sans-serif" size="2">disco 
-            usado</font></td>
-          <td width="21%" bgcolor="#D6D6D6"><font face="Arial, Helvetica, sans-serif" size="2">tr&aacute;fico</font></td>
+          <td width="61%" bgcolor="#D6D6D6">nombre</td>
+          <td width="18%" bgcolor="#D6D6D6">disco 
+            usado</td>
+          <td width="21%" bgcolor="#D6D6D6">tr&aacute;fico</td>
         </tr>
         <tr align="center"> 
           <td colspan="3">
             <table width="100%" border="0" cellspacing="5" cellpadding="0">
               <tr align="center"> 
-                <td width="61%" align="left"><b><font face="Arial, Helvetica, sans-serif" size="2" color="#D65300">&nbsp;fidojones.com</font></b></td>
-                <td width="18%" align="right"><b><font face="Arial, Helvetica, sans-serif" size="2">20.00&nbsp;</font></b></td>
-                <td width="21%" align="right"><b><font face="Arial, Helvetica, sans-serif" size="2">5.00&nbsp;</font></b></td>
+                <td width="61%" align="left"><span class="Estilo5"><font color="#D65300">&nbsp;fidojones.com</font></span></td>
+                <td width="18%" align="right"><strong><span class="Estilo6">20.00&nbsp;</span></strong></td>
+                <td width="21%" align="right"><span class="Estilo5"><b>5.00</b></span><b>&nbsp;</b></td>
               </tr>
               <tr align="center"> 
-                <td width="61%" align="left"><b><font face="Arial, Helvetica, sans-serif" size="2" color="#D65300">&nbsp;esdominios.com</font></b></td>
-                <td width="18%" align="right"><b><font face="Arial, Helvetica, sans-serif" size="2">10.50&nbsp;</font></b></td>
-                <td width="21%" align="right"><b><font face="Arial, Helvetica, sans-serif" size="2">13.00&nbsp;</font></b></td>
+                <td width="61%" align="left" class="Estilo6"><span class="Estilo5"><font color="#D65300">&nbsp;esdominios.com</font></span></td>
+                <td width="18%" align="right"><span class="Estilo5"><b>10.50&nbsp;</b></span></td>
+                <td width="21%" align="right" class="Estilo5"><b>13.00&nbsp;</b></td>
               </tr>
               <tr align="center"> 
-                <td width="61%" align="left"><b><font face="Arial, Helvetica, sans-serif" size="2" color="#D65300">&nbsp;hostaplus.com</font></b></td>
-                <td width="18%" align="right"><b><font face="Arial, Helvetica, sans-serif" size="2">35.10&nbsp;</font></b></td>
-                <td width="21%" align="right"><b><font face="Arial, Helvetica, sans-serif" size="2">100.00&nbsp;</font></b></td>
+                <td width="61%" align="left" class="Estilo5"><font color="#D65300">&nbsp;hostaplus.com</font></td>
+                <td width="18%" align="right"><strong class="Estilo5">35.10&nbsp;</strong></td>
+                <td width="21%" align="right"><b class="Estilo5">100.00&nbsp;</b></td>
               </tr>
             </table>
           </td>
         </tr>
         <tr align="center"> 
-          <td colspan="2"><font face="Arial, Helvetica, sans-serif" size="2"><b>0 
-            Dominios en total</b></font></td>
+          <td colspan="2">0 
+            Dominios en total</td>
           <td width="21%">&nbsp;</td>
         </tr>
       </table>
@@ -95,30 +103,30 @@
             <table width="100%" border="0" cellspacing="0" cellpadding="0" background="images/fnd_disco.gif">
               <tr> 
                 <td width="15%" align="center">&nbsp;</td>
-                <td width="85%"><font face="Arial, Helvetica, sans-serif" size="1"><b><font size="3" color="#FFFFFF">Espacio 
-                  en disco</font><font size="2" color="#FFFFFF"><br>
-                  consumo y disponibilidad</font></b></font></td>
+                <td width="85%"><font face="Arial, Helvetica, sans-serif" size="1"><b><font color="#FFFFFF" size="3" face="Verdana, Arial, Helvetica, sans-serif">Espacio 
+                  en disco</font><font color="#FFFFFF"><br>
+                  </font></b></font><font size="1"><font color="#FFFFFF"><font size="1">consumo y disponibilidad</font></font></font><font size="1">&nbsp;</font></td>
               </tr>
             </table>
             <table width="100%" border="0" cellspacing="5" cellpadding="0">
               <tr align="center"> 
-                <td width="46%" align="left"><b><font face="Arial, Helvetica, sans-serif" size="2">&nbsp;Espacio 
-                  en disco</font></b></td>
+                <td width="46%" align="left" class="Estilo2">&nbsp;Espacio 
+                  en disco</td>
                 <td width="34%"><img src="images/barra_progreso.gif" width="120" height="11"></td>
               </tr>
               <tr align="center"> 
-                <td width="46%" align="left"><b><font face="Arial, Helvetica, sans-serif" size="2">&nbsp;Espacio 
-                  total </font></b></td>
-                <td width="34%" align="right"><b><font face="Arial, Helvetica, sans-serif" size="2">500&nbsp;</font></b></td>
+                <td width="46%" align="left" class="Estilo2">&nbsp;Espacio 
+                  total </td>
+                <td width="34%" align="right"><b>500<font size="1"></font><font size="1"></font><font face="Verdana, Arial, Helvetica, sans-serif" size="1">&nbsp;</font></b></td>
               </tr>
               <tr align="center"> 
-                <td width="46%" align="left"><b><font face="Arial, Helvetica, sans-serif" size="2">&nbsp;Espacio 
-                  Usado</font></b></td>
-                <td width="34%" align="right"><b><font face="Arial, Helvetica, sans-serif" size="2">50&nbsp;</font></b></td>
+                <td width="46%" align="left" class="Estilo2">&nbsp;Espacio 
+                  Usado</td>
+                <td width="34%" align="right"><b>50<font size="2"></font><font size="2"></font><font face="Verdana, Arial, Helvetica, sans-serif" size="2">&nbsp;</font></b></td>
               </tr>
               <tr align="center"> 
-                <td width="46%" align="left"><b><font face="Arial, Helvetica, sans-serif" size="2">&nbsp;Disponible</font></b></td>
-                <td width="34%" align="right"><b><font face="Arial, Helvetica, sans-serif" size="2">50&nbsp;</font></b></td>
+                <td width="46%" align="left"><b>&nbsp;</b>Disponible</td>
+                <td width="34%" align="right"><b>50<font size="2"></font><font size="2"></font><font face="Verdana, Arial, Helvetica, sans-serif" size="2">&nbsp;</font></b></td>
               </tr>
             </table>
           </td>
@@ -131,29 +139,29 @@
             <table width="100%" border="0" cellspacing="0" cellpadding="0" background="images/fnd_trafico.gif">
               <tr> 
                 <td width="15%" align="center">&nbsp;</td>
-                <td width="85%"><font face="Arial, Helvetica, sans-serif" size="1"><b><font size="3" color="#FFFFFF">Transferencia</font><font size="2" color="#FFFFFF"><br>
-                  Transferencia de Datos</font></b></font></td>
+                <td width="85%"><font face="Arial, Helvetica, sans-serif" size="1"><b><font color="#FFFFFF" size="3" face="Verdana, Arial, Helvetica, sans-serif">Transferencia</font><font color="#FFFFFF" size="2" face="Verdana, Arial, Helvetica, sans-serif"><br>
+                  </font></b></font><font size="1"><font color="#FFFFFF">Transferencia de Datos</font></font><font size="1">&nbsp;</font></td>
               </tr>
             </table>
             <table width="100%" border="0" cellspacing="5" cellpadding="0">
               <tr align="center"> 
-                <td width="46%" align="left"><b><font face="Arial, Helvetica, sans-serif" size="2">&nbsp;Transferencia</font></b></td>
+                <td width="46%" align="left">&nbsp;Transferencia</td>
                 <td width="34%"><img src="images/barra_progreso.gif" width="120" height="11"></td>
               </tr>
               <tr align="center"> 
-                <td width="46%" align="left"><b><font face="Arial, Helvetica, sans-serif" size="2">&nbsp;L&iacute;mite 
-                  Mensual </font></b></td>
-                <td width="34%" align="right"><b><font face="Arial, Helvetica, sans-serif" size="2">1000&nbsp;</font></b></td>
+                <td width="46%" align="left">&nbsp;L&iacute;mite 
+                  Mensual </td>
+                <td width="34%" align="right"><b>1000<font size="2"></font><font size="2"></font><font face="Verdana, Arial, Helvetica, sans-serif" size="2">&nbsp;</font></b></td>
               </tr>
               <tr align="center"> 
-                <td width="46%" align="left"><b><font face="Arial, Helvetica, sans-serif" size="2">&nbsp;&Uacute;ltimo 
-                  Mes </font></b></td>
-                <td width="34%" align="right"><b><font face="Arial, Helvetica, sans-serif" size="2">50&nbsp;</font></b></td>
+                <td width="46%" align="left">&nbsp;&Uacute;ltimo 
+                  Mes </td>
+                <td width="34%" align="right"><b>50<font size="2"></font><font size="2"></font><font face="Verdana, Arial, Helvetica, sans-serif" size="2">&nbsp;</font></b></td>
               </tr>
               <tr align="center"> 
-                <td width="46%" align="left"><b><font face="Arial, Helvetica, sans-serif" size="2">&nbsp;Mes 
-                  actual </font></b></td>
-                <td width="34%" align="right"><b><font face="Arial, Helvetica, sans-serif" size="2">50&nbsp;</font></b></td>
+                <td width="46%" align="left"><font face="Verdana, Arial, Helvetica, sans-serif">&nbsp;Mes 
+                  actual</font></td>
+                <td width="34%" align="right"><b>50<font size="2"></font><font size="2"></font><font face="Verdana, Arial, Helvetica, sans-serif" size="2">&nbsp;</font></b></td>
               </tr>
             </table>
           </td>
