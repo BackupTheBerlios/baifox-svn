feaa5d5960d771adf7bd751b61524e873b2ec8de
fidojones <fidojones@51d618aa-2c00-0410-be48-da876a5fb5c8>
2006-03-05 18:41:06 +0000

    soporte de actualizacion finalizado

--- trunk/panel/admin_panel/update/main.php
+++ trunk/panel/admin_panel/update/main.php
@@ -5,15 +5,22 @@
 <?php include "include_permiso.php"; ?>
 <?php 
  function download($fichero_origen,$ruta_origen, $fichero_destino) {
-        $origen = fopen("http://update.baifox.net/baifox_download.php?file=$fichero_origen&path=$ruta_origen", 'rb');
-        $destino = fopen($file_target, 'wb');
-        if ($origen===false || $destino===false) {
+        $origen = fopen("http://update.baifox.net/baifox_download.php?file=$fichero_origen&path=$ruta_origen", "rb");
+        $destino = fopen($fichero_destino, 'wb');
+        if ($origen===false) {
  	    // error leyendo el fichero
+	    echo "[ERROR] leyendo fichero origen<br>\n";
+            return false;
+        }
+        if ($destino===false) {
+ 	    // error leyendo el fichero
+	    echo "[ERROR] leyendo fichero destino<br>\n";
             return false;
         }
         while (!feof($origen)) {
-            if (fwrite($destino, fread($origen, 1024)) === FALSE) {
+            if (fwrite($destino, fread($origen, 4096)) === FALSE) {
 		    // error escribiendo el fichero
+	    	   echo "[ERROR] escribiendo fichero destino<br>\n";
                     return false;
                 }
         }
@@ -21,22 +28,96 @@
         fclose($destino);
         return true;
     }
+    function update_version($fichero_destino,$version){
+	$origen = file($fichero_destino);
+        $destino = fopen($fichero_destino, 'wb');
+    	if ($destino===false) {
+ 		// error leyendo el fichero
+	   	echo "[ERROR] leyendo fichero destino<br>\n";
+           	return false;
+        }
+	foreach ($origen as $linea){
+		if(strpos($linea,"_CFG_INTERFACE_VERSION")!==false){
+			if (fwrite($destino, "define(\"_CFG_INTERFACE_VERSION\",\"$version\");") === FALSE) {
+				// error escribiendo el fichero
+	   			echo "[ERROR] escribiendo fichero destino<br>\n";
+           			return false;
+           		}
+		}else{
+            		if (fwrite($destino, $linea) === FALSE) {
+				// error escribiendo el fichero
+	    	 		echo "[ERROR] escribiendo fichero destino<br>\n";
+                 		return false;
+                	}
+		}
+    	}
+	fclose($destino);
+    }
+    function update_config($fichero_origen,$ruta_origen, $fichero_destino) {
+		$lines_origen = file("http://update.baifox.net/baifox_download.php?file=$fichero_origen&path=$ruta_origen");
+		$lines_destino = file($fichero_destino);
+	 	foreach ($lines_origen as $linea_origen) {
+			$encontrado=false;
+	    		if(ereg("(define[(]\")(.*)(\",)", $linea_origen, $registros_origen)){
+				foreach ($lines_destino as $linea_destino){
+					if(ereg("(define[(]\")(.*)(\",)", $linea_destino, $registros_destino)){
+						if($registros_origen[2]==$registros_destino[2])
+							$encontrado=true;
+					}
+				}
+				if(!$encontrado){
+					$origen = file($fichero_destino);
+        				$destino = fopen($fichero_destino, 'wb');
+    					if ($destino===false) {
+ 	    					// error leyendo el fichero
+	    					echo "[ERROR] leyendo fichero destino<br>\n";
+            					return false;
+        				}
+					foreach ($origen as $linea){
+						if(strpos($linea,"?>")!==false){
+							if (fwrite($destino, $linea_origen) === FALSE) {
+			    					// error escribiendo el fichero
+	    	   						echo "[ERROR] escribiendo fichero destino<br>\n";
+                    						return false;
+                					}
+							if (fwrite($destino, "?>") === FALSE) {
+			    					// error escribiendo el fichero
+	    	   						echo "[ERROR] escribiendo fichero destino<br>\n";
+                    						return false;
+                					}
+						}else{
+            						if (fwrite($destino, $linea) === FALSE) {
+			    					// error escribiendo el fichero
+	    	   						echo "[ERROR] escribiendo fichero destino<br>\n";
+                    						return false;
+                					}
+						}
+        				}
+				        fclose($destino);
+				}
+			}
+		}
+    }
 ?>
 <html>
 <head>
-<title>Untitled Document</title>
+<title>Baifox Actualización Manual</title>
 <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
 </head>
 
 <body bgcolor="#FFFFFF" text="#000000">
+<font face="Verdana, Arial, Helvetica, sans-serif" size="2">
 <?php
 	if($_GET['actualizar']=="ok"){	
+		$lines = file("http://update.baifox.net/version.cfg");
+		$nueva_version=$lines[0];
+		$fichero_config="";
 		$lines = file("http://update.baifox.net/update.cfg");
 	 
 	 	foreach ($lines as $linea) {
     			list($operacion,$tipo,$tam,$fichero,$ruta) =explode("][",substr($linea,1,strlen($linea)-3));
 			$ruta_destino=str_replace("/panel/","",_CFG_INTERFACE_DIR).$ruta."/".$fichero;
-			echo "----------------------------------------------<br><br>\n";
+			echo "----------------------------------------------<br>\n";
 			if($tipo=="DIR"){
 				if(file_exists($ruta_destino)){
 					echo "Directorio ya existe $ruta_destino<br>\n";
@@ -47,21 +128,36 @@
 			}else{
 				switch($operacion){
 				case "O":
-					echo "Sobreescribiendo fichero $ruta_destino<br>\n";
 					if(download($fichero,$ruta,$ruta_destino))
+						echo "Sobreescribiendo fichero $ruta_destino<br>\n";
+					else
 						echo "[ERROR] Sobreescribiendo fichero $ruta_destino<br>\n";
 				break;
 				case "X":
 					echo "Fichero sin modificar $ruta_destino<br>\n";
 				break;
 				case "U":
+					if($fichero=="main_config.php"){
+						$fichero_config=$ruta_destino;
+						update_config($fichero,$ruta,$ruta_destino);
+					}
 					echo "Fichero actualizado $ruta_destino<br>\n";
 				break;
 				}
 			}
 			flush();
- 		}
-	}else{ ?>
+ 		} 
+		update_version($fichero_config,$nueva_version);
+		echo "<br>----------------------------------------------<br>\n";
+		echo "<b>Actualización finalizada</b><br>\n";
+		?>
+<SCRIPT language="JavaScript" type="text/javascript">
+ <!--
+     	parent.cabecera.location.href="top.php";
+//-->
+</SCRIPT>
+</font>
+<?php	}else{ ?>
 <table width="80%" border="0" cellspacing="0" cellpadding="0" align="center">
   <tr>
     <td align="center" bgcolor="#FFCC66"><font face="Verdana, Arial, Helvetica, sans-serif" size="2"><b>&Uacute;ltimas 
