ebfe522190d9080f31a94ba06d008ae41a77cfc2
fidojones <fidojones@51d618aa-2c00-0410-be48-da876a5fb5c8>
2006-05-17 18:42:23 +0000

    soporte de cantidades 'infinitas'

--- trunk/panel/secure.php
+++ trunk/panel/secure.php
@@ -25,9 +25,18 @@
 	 			$_SESSION['SEC_PERM']=$rs["PERMISO"];
 	 			$_SESSION['SEC_USER_NOMBRE']=$rs["NOMBRE"];
 	 			$_SESSION['SEC_USER_EMAIL']=$rs["EMAIL"];
-				$_SESSION['SEC_USER_TOTAL_DOMINIOS']=$rs["DOMINIOS"];
-				$_SESSION['SEC_USER_TOTAL_ESPACIO']=$rs["ESPACIO"];
-				$_SESSION['SEC_USER_TOTAL_ANCHOBANDA']=$rs["ANCHOBANDA"];
+				if($rs["DOMINIOS"]<=0)
+					$_SESSION['SEC_USER_TOTAL_DOMINIOS']=9999;
+				else
+					$_SESSION['SEC_USER_TOTAL_DOMINIOS']=$rs["DOMINIOS"];
+				if($rs["ESPACIO"]<=0)
+					$_SESSION['SEC_USER_TOTAL_ESPACIO']=99999;
+				else
+					$_SESSION['SEC_USER_TOTAL_ESPACIO']=$rs["ESPACIO"];
+				if($rs["ANCHOBANDA"]<=0)
+					$_SESSION['SEC_USER_TOTAL_ANCHOBANDA']=999999;
+				else
+					$_SESSION['SEC_USER_TOTAL_ANCHOBANDA']=$rs["ANCHOBANDA"];
 				$_SESSION['SEC_USER_DOMINIOS']=xmlconfig_arraydominios($rs["ID"]);
 				header ("Location: user_panel/index.php\n\n");
 				exit();
